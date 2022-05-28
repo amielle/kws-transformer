209ef5009197a2b6ef49411322c7da46f959c25e
amielle <amielle.dulay@gmail.com>
2022-05-28 13:45:39 +0800

    feat: Add dataloader module

---
+++ utils/dataloader.py
@@ -0,0 +1,143 @@
+import torch
+import torchaudio, torchvision
+import os
+import matplotlib.pyplot as plt 
+import librosa
+import argparse
+import numpy as np
+import wandb
+from pytorch_lightning import LightningModule, Trainer, LightningDataModule, Callback
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+from torchmetrics.functional import accuracy
+from torchvision.transforms import ToTensor
+from torchaudio.datasets import SPEECHCOMMANDS
+from torchaudio.datasets.speechcommands import load_speechcommands_item
+
+
+class SilenceDataset(SPEECHCOMMANDS):
+    def __init__(self, root):
+        super(SilenceDataset, self).__init__(root, subset='training')
+        self.len = len(self._walker) // 35
+        path = os.path.join(self._path, torchaudio.datasets.speechcommands.EXCEPT_FOLDER)
+        self.paths = [os.path.join(path, p) for p in os.listdir(path) if p.endswith('.wav')]
+
+    def __getitem__(self, index):
+        index = np.random.randint(0, len(self.paths))
+        filepath = self.paths[index]
+        waveform, sample_rate = torchaudio.load(filepath)
+        return waveform, sample_rate, "silence", 0, 0
+
+    def __len__(self):
+        return self.len
+
+class UnknownDataset(SPEECHCOMMANDS):
+    def __init__(self, root):
+        super(UnknownDataset, self).__init__(root, subset='training')
+        self.len = len(self._walker) // 35
+
+    def __getitem__(self, index):
+        index = np.random.randint(0, len(self._walker))
+        fileid = self._walker[index]
+        waveform, sample_rate, _, speaker_id, utterance_number = load_speechcommands_item(fileid, self._path)
+        return waveform, sample_rate, "unknown", speaker_id, utterance_number
+
+    def __len__(self):
+        return self.len
+
+
+class KWSDataModule(LightningDataModule):
+    def __init__(self, path, batch_size=32, num_workers=0, n_fft=512, 
+                 n_mels=128, win_length=None, hop_length=256, patch_num=4, class_dict={}, 
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.path = path
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.n_fft = n_fft
+        self.n_mels = n_mels
+        self.win_length = win_length
+        self.hop_length = hop_length
+        self.class_dict = class_dict
+        self.patch_num = patch_num
+
+    def prepare_data(self):
+        self.train_dataset = torchaudio.datasets.SPEECHCOMMANDS(self.path,
+                                                                download=True,
+                                                                subset='training')
+
+        silence_dataset = SilenceDataset(self.path)
+        unknown_dataset = UnknownDataset(self.path)
+        self.train_dataset = torch.utils.data.ConcatDataset([self.train_dataset, silence_dataset, unknown_dataset])
+                                                                
+        self.val_dataset = torchaudio.datasets.SPEECHCOMMANDS(self.path,
+                                                              download=True,
+                                                              subset='validation')
+        self.test_dataset = torchaudio.datasets.SPEECHCOMMANDS(self.path,
+                                                               download=True,
+                                                               subset='testing')                                                    
+        _, sample_rate, _, _, _ = self.train_dataset[0]
+        self.sample_rate = sample_rate
+        self.transform = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
+                                                              n_fft=self.n_fft,
+                                                              win_length=self.win_length,
+                                                              hop_length=self.hop_length,
+                                                              n_mels=self.n_mels,
+                                                              power=2.0)
+
+    def setup(self, stage=None):
+        self.prepare_data()
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=True,
+            collate_fn=self.collate_fn
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=True,
+            collate_fn=self.collate_fn
+        )
+    
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=True,
+            collate_fn=self.collate_fn
+        )
+
+    def collate_fn(self, batch):
+        mels = []
+        labels = []
+        wavs = []
+        for sample in batch:
+            waveform, sample_rate, label, speaker_id, utterance_number = sample
+            # ensure that all waveforms are 1sec in length; if not pad with zeros
+            if waveform.shape[-1] < sample_rate:
+                waveform = torch.cat([waveform, torch.zeros((1, sample_rate - waveform.shape[-1]))], dim=-1)
+            elif waveform.shape[-1] > sample_rate:
+                waveform = waveform[:,:sample_rate]
+
+            # mel from power to db
+            mels.append(ToTensor()(librosa.power_to_db(self.transform(waveform).squeeze().numpy(), ref=np.max)))
+            labels.append(torch.tensor(self.class_dict[label]))
+            wavs.append(waveform)
+
+        mels = torch.stack(mels)
+        mels = rearrange(mels, 'b c (p1 h) (p2 w) -> b (p1 p2) (c h w)', p1=self.patch_num, p2=self.patch_num)
+        labels = torch.stack(labels)
+        wavs = torch.stack(wavs)
+   
+        return mels, labels, wavs
