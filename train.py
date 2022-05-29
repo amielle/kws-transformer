000c079c9a32b45141668b4a7438654dd7d02c65
amielle <amielle.dulay@gmail.com>
2022-05-29 10:24:40 +0800

    chore: Move handler to train
    chore: Remove isolated argument module in utils

--- train.py
+++ train.py
@@ -11,6 +11,47 @@ from utils.arghandler import get_args
 from utils.dataloader import KWSDataModule
 from utils.model import KWSTransformer
 
+def get_args(parser):
+    # model training hyperparameters
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--max-epochs', type=int, default=1, metavar='N',
+                        help='number of epochs to train (default: 30)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+
+    # where dataset will be stored
+    parser.add_argument("--path", type=str, default="data/speech_commands/")
+
+    # 35 keywords + silence + unknown
+    parser.add_argument("--num-classes", type=int, default=37)
+   
+    # mel spectrogram parameters
+    parser.add_argument("--n-fft", type=int, default=1024)
+    parser.add_argument("--n-mels", type=int, default=128)
+    parser.add_argument("--win-length", type=int, default=None)
+    parser.add_argument("--hop-length", type=int, default=512)
+
+    # 16-bit fp model to reduce the size
+    parser.add_argument("--precision", default=16)
+    parser.add_argument("--accelerator", default='gpu')
+    parser.add_argument("--devices", default=1)
+    parser.add_argument("--num-workers", type=int, default=2)
+
+    # transformer arguments
+    parser.add_argument('--depth', type=int, default=32, help='depth')
+    parser.add_argument('--embed_dim', type=int, default=64, help='embedding dimension')
+    parser.add_argument('--num_heads', type=int, default=16, help='num_heads')
+
+    parser.add_argument('--patch_num', type=int, default=4, help='patch_num')
+    parser.add_argument('--kernel_size', type=int, default=3, help='kernel size')
+    
+    parser.add_argument("--no-wandb", default=True, action="store_true")
+
+    args = parser.parse_args("")
+    return args
+
+
 class WandbCallback(Callback):
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
